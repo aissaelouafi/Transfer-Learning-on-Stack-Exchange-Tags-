65b829cab159126cd130dafb175e94d5e3f5040c
aissaelouafi <aissaelouafi1994@gmail.com>
2016-11-03 13:28:26 +0100

    Explore acp result for a small travel dataset. Build a naive bayes classifier with a small topic dataset (we take in the exmaple the travel dataset only 1000 rows to construct the model and 1000 rows to evaluate the model).

--- TagsPrediction.R
+++ TagsPrediction.R
@@ -9,6 +9,10 @@ library(tm)
 library(wordcloud)
 library(e1071) #Naive bayes classifier 
 library(stats)
+library(factoextra)
+
+#Get dev sparlyr version
+devtools::install_github("rstudio/sparklyr")
 
 #Remove all from memory
 rm(list=ls())
@@ -18,7 +22,9 @@ sc <- spark_connect(master="local")
 
 # Import Data
 biology =  read.csv("../biology.csv")
-travel = read.csv("../travel.csv")
+travel = read.csv("../travel.csv",nrows=1000) # Get only first 1000 rows to construct the model for memory issues
+travel_test = read.csv("../travel.csv",nrows = 1000 ,skip= 18279) #Predict on last 1000 rows of travel
+colnames(travel_test) <- colnames(travel)
 robotic = read.csv("../robotics.csv")
 cooking = read.csv("../cooking.csv")
 crypto = read.csv("../crypto.csv")
@@ -28,7 +34,7 @@ sample_submission = read.csv("../sample_submission.csv")
 
 #Copy data to spark environment 
 #biology_sc <- as.data.frame(biology)
-#scc <- copy_to(sc,biology_sc)
+cc <- copy_to(sc,travel)
 
 ############################################################
 #                                                          #
@@ -66,6 +72,23 @@ titleTagsProbability <- function(df){
   return(table(tag_in_title)["TRUE"] / length(tag_in_title))
 }
 
+
+# Prepare the corpus from an input dataset dataframe
+# input : df : the dataframe, column : df column to construct the Corpus
+# output : tfidfx matrix 
+
+generateCorpus <- function(df){
+  corpus <- Corpus(VectorSource(df))
+  replacePunctuation <- content_transformer(function(x) {return (gsub("[[:punct:]]"," ", x))})
+  corpus <- tm_map(corpus,replacePunctuation)
+  corpus <- tm_map(corpus,removePunctuation)
+  corpus <- tm_map(corpus,removeNumbers)
+  corpus <- tm_map(corpus,tolower)
+  corpus <- tm_map(corpus,removeWords,stopwords("english"))
+  corpus <- tm_map(corpus, stripWhitespace) 
+  corpus <- tm_map(corpus, PlainTextDocument)
+  return(corpus)
+}
 ############################################################
 #                                                          #
 #                   DATA VIZUALISATION                     #  
@@ -73,7 +96,7 @@ titleTagsProbability <- function(df){
 ############################################################
 
 #Count tags frequency by topic and vizualise the bar chart of most frequent tags
-travel_tags_chart <- countDistinctTags(travel,50)
+travel_tags_chart <- countDistinctTags(travel,10)
 biology_tags_chart <- countDistinctTags(biology,50)
 cooking_tags_chart <- countDistinctTags(cooking,10)
 
@@ -86,17 +109,11 @@ titleTagsProbability(cooking)
 ############################################################
 
 # Topic titles corpus prepocessing
-titles_corpus <- Corpus(VectorSource(travel$title))
-replacePunctuation <- content_transformer(function(x) {return (gsub("[[:punct:]]"," ", x))})
-titles_corpus <- tm_map(titles_corpus,replacePunctuation)
-titles_corpus <- tm_map(titles_corpus,removePunctuation)
-titles_corpus <- tm_map(titles_corpus,removeNumbers)
-titles_corpus <- tm_map(titles_corpus,tolower)
-titles_corpus <- tm_map(titles_corpus,removeWords,stopwords("english"))
-titles_corpus <- tm_map(titles_corpus, stripWhitespace) 
-titles_corpus <- tm_map(titles_corpus, PlainTextDocument) 
+titles_corpus <- generateCorpus(travel$title)
+title_corpus_test <- generateCorpus(travel_test$title)
+tfidf_test <- DocumentTermMatrix(title_corpus_test, control = list(weighting = weightTfIdf))
 
-# Create the document-term matrix and the tf-idf matrix
+# Create the document-term matrix and the tf-idf matrix for both train and test data 
 dtm <- DocumentTermMatrix(titles_corpus)
 tfidf <- DocumentTermMatrix(titles_corpus, control = list(weighting = weightTfIdf))
 freq <- sort(colSums(as.matrix(dtm)),decreasing = TRUE)
@@ -107,17 +124,83 @@ p <- plot_ly(x = wf$word, y = wf$freq,type="bar") %>%
   layout(yaxis = list(title = "Freq"),xaxis = list(title = "Topic title"))
 
 #Word cloud of topic titles & Word cloud of topic tags
-wordcloud(names(freq),freq,min.freq = 250)
+wordcloud(names(freq),freq,min.freq = 10)
 
 #Convert the tfidf matrix to dataframe
 tfidf <- as.data.frame(as.matrix(tfidf))
 
 #Analyze a small part from the tfidf matrix for test
-tfidf_small <- tfidf[1:100,]
-rownames(tfidf_small) <- 1:nrow(tfidf_small)
+rownames(tfidf) <- 1:nrow(tfidf)
 
-#Dimensionality reduction : PCA Analysis 
-log.tfidf <- log(tfidf)
+#ANNEX : Copy the tfidf matrix into spark context
+tfidf_sc <- copy_to(sc,tfidf)
+
+
+
+#Dimensionality reduction : PCA Analysis based on the small tfidf dataframe
+tfidf.pca <- prcomp(tfidf)
+names(tfidf.pca)
+head(tfidf.pca$sdev)
+
+#Study variances of the principal components
+#Eigenvalues (valeur propre): Eigenvalues measures the variability retained by each PC. It's large for the first PC and small for the subsequent PCs.
+eig <- (tfidf.pca$sdev)^2
+#Variance
+variance <- eig*100/sum(eig)
+#Cumulative variance
+cumvariance <- cumsum(variance) # The total of the cumulative variance should be egal to 100
+
+#Let's construct a dataframe with all this informations 
+eig.dataframe <- data.frame(eig = eig, variance = variance, cumvariance = cumvariance)
+
+#See the importance of principal composents (PCs) using a scree plot
+eig.dataframe <- eig.dataframe[1:50,]
+barplot(eig.dataframe[,2],
+        names.arg = 1:nrow(eig.dataframe),
+        main="Variance scree plot",
+        xlab = "Principal Component",
+        ylab = "Percentage of the variance",
+        col = "steelblue")
+
+lines(x=1:nrow(eig.dataframe),eig.dataframe[,2],type="b",pch=10, col="red")
+
+#Another alternative to plot with using the factoextra library
+fviz_screeplot(tfidf.pca, ncp=10)
+
+# Note that a good dimension reduction is achieved when the first few PCs account for a large porportion of the variability (80-90%)
+fviz_pca_var(tfidf.pca)
+
+#Graph of variables (The correlation circle)
+#In order to extract the results for variables contributions in princal components of the PCA
+#using get_pca_var we get a list of matrices containing all results for active variables (cordinates, correlation between variables and axes, squared cosinus and contributions)
+var_contr <- get_pca_var(tfidf.pca)
+
+#let show the variables contributions to construct the 10 first PCs of our 
+var_contr_dim <- as.data.frame(var_contr$contrib)[,1:10]
+
+#let show the cordinates of variables in the first 4 PCs
+var_cordinates <- as.data.frame(var_contr$coord)[1:10,1:4]
+
+#let plot the correlation circle of variables with the PCs
+a <- seq(0,2*pi, length = 100)
+plot(cos(a),sin(a),type = 'l', color = 'gray',xlab = "PC1", ylab = "PC2")
+abline(h = 0, v = 0, lty = 2)
+arrows(0,0,var_cordinates[,1], var_cordinates[,2], length = 0.5, angle = 15, code = 2)
+text(var_cordinates, labels=rownames(var_cordinates), cex = 1, adj=1)
+
+
+fviz_pca_var(tfidf.pca)
 
 #Build the naive bayes classifier 
 model <- naiveBayes(as.matrix(tfidf),as.factor(travel$tags))
+
+prediction <- predict(model,as.matrix(tfidf_test))
+colnames(prediction) <- c("tags")
+prediction <- as.data.frame(prediction)
+
+#Show the most frequent predicted tags 
+frequent_predicted_tags <- as.data.frame(table(prediction))
+frequent_predicted_tags <- frequent_predicted_tags[order(frequent_predicted_tags$Freq,decreasing = TRUE),]
+frequent_predicted_tags <- frequent_predicted_tags[1:10,]
+p <- plot_ly(x = frequent_predicted_tags$prediction, y = frequent_predicted_tags$Freq,type="bar") %>%
+  layout(yaxis = list(title = "Freq"),xaxis = list(title = "Tags"))
